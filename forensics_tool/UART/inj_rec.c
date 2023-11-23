ddb9a26740649713679b4a57daff31351c623a46
Kai <51684541+Kai-i-i-i@users.noreply.github.com>
2023-11-23 14:25:59 +0800

    added test code

--- forensics_tool/UART/inj_rec.c
+++ forensics_tool/UART/inj_rec.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <string.h>
 #include "hardware/gpio.h"
 #include "hardware/uart.h"
 #include "pico/stdlib.h"
@@ -20,7 +19,7 @@
 #define UART_RX_PIN_TESTING 9
 
 // Setting pin 15 as a pseudo button.
-const uint GP15_PIN = 15;
+//const uint GP15_PIN = 15;
 
 // Define the maximum length of strings to be injected or sniffed
 const int MAX_STRING_LENGTH = 100;
@@ -29,22 +28,26 @@ const int MAX_STRING_LENGTH = 100;
 char *injectString = "Hello from Pico!\n";
 
 // Function to send data based on GP15's state
-void inject() {
-  // When GP15 is high, send the string 'Hello from Pico!' through UART0.
+void inject()
+{
+  // When inject mode is selected send the string 'Hello from Pico!' through UART0.
   uart_puts(UART_ID, injectString);
-  sleep_ms(200);  // Add a small delay after sending the string for consistency
+  sleep_ms(200); // Add a small delay after sending the string for consistency
 }
 
-void sniff() {
-  // When GP15 is low, enter sniffing mode.
+void sniff()
+{
+  // When menu selected sniffing mode.
   char receivedString[MAX_STRING_LENGTH];
   int receivedStringIndex = 0;
 
-  while (uart_is_readable(UART_ID)) {
+  while (uart_is_readable(UART_ID))
+  {
     char receivedChar = uart_getc(UART_ID);
 
     // Check if the received character is a newline character
-    if (receivedChar == '\n') {
+    if (receivedChar == '\n')
+    {
       // If a newline character is received, print the received string
       printf("Received string: %s\n", receivedString);
 
@@ -54,12 +57,15 @@ void sniff() {
       // Reset the received string index and received string
       receivedStringIndex = 0;
       memset(receivedString, 0, sizeof(receivedString));
-    } else {
+    }
+    else
+    {
       // Append the received character to the received string
       receivedString[receivedStringIndex++] = receivedChar;
 
       // Check if the received string index reaches the maximum length
-      if (receivedStringIndex >= MAX_STRING_LENGTH) {
+      if (receivedStringIndex >= MAX_STRING_LENGTH)
+      {
         // If the maximum length is reached, reset the received string index
         // and received string
         receivedStringIndex = 0;
@@ -70,110 +76,127 @@ void sniff() {
 }
 
 // Function to test UART1 communication
-void test_uart1_tx() {
-    char *testString = "Hello from Pin 8!\n";
-    uart_puts(uart1, testString); // Change back to UART0 later
-    sleep_ms(200);
-}
+// void test_uart1_tx()
+// {
+//   char *testString = "Hello from Pin 8!\n";
+//   uart_puts(uart1, testString); // Change back to UART0 later
+//   sleep_ms(200);
+// }
 
 // Function to test UART1 communication
-void test_uart1_rx() {
-  char receivedString[MAX_STRING_LENGTH];
-  int receivedStringIndex = 0;
+// void test_uart1_rx()
+// {
+//   char receivedString[MAX_STRING_LENGTH];
+//   int receivedStringIndex = 0;
 
-  while (uart_is_readable(uart1)) {
-    char receivedChar = uart_getc(uart1);
+//   while (uart_is_readable(uart1))
+//   {
+//     char receivedChar = uart_getc(uart1);
 
-    // Check if the received character is a newline character
-    if (receivedChar == '\n') {
-      // If a newline character is received, print the received string
-      printf("Received from PIN9: %s\n", receivedString);
+//     // Check if the received character is a newline character
+//     if (receivedChar == '\n')
+//     {
+//       // If a newline character is received, print the received string
+//       printf("Received from PIN9: %s\n", receivedString);
 
-      // Reset the received string index and received string
-      receivedStringIndex = 0;
-      memset(receivedString, 0, sizeof(receivedString));
-    } else {
-      // Append the received character to the received string
-      receivedString[receivedStringIndex++] = receivedChar;
+//       // Reset the received string index and received string
+//       receivedStringIndex = 0;
+//       memset(receivedString, 0, sizeof(receivedString));
+//     }
+//     else
+//     {
+//       // Append the received character to the received string
+//       receivedString[receivedStringIndex++] = receivedChar;
 
-      // Check if the received string index reaches the maximum length
-      if (receivedStringIndex >= MAX_STRING_LENGTH) {
-        // If the maximum length is reached, reset the received string index and
-        // received string
-        receivedStringIndex = 0;
-        memset(receivedString, 0, sizeof(receivedString));
-      }
-    }
-  }
-  sleep_ms(500);
-}
+//       // Check if the received string index reaches the maximum length
+//       if (receivedStringIndex >= MAX_STRING_LENGTH)
+//       {
+//         // If the maximum length is reached, reset the received string index and
+//         // received string
+//         receivedStringIndex = 0;
+//         memset(receivedString, 0, sizeof(receivedString));
+//       }
+//     }
+//   }
+//   sleep_ms(500);
+// }
 
-int main() {
+int main()
+{
   // Initialize.
   stdio_init_all();
 
-  //menu tracker things
+  // menu tracker things
   char menu_buffer[1024];
   int in_menu = 0;
   int menu_activated = 1;
 
   // Initialize UART0 (uart0)
   uart_init(UART_ID, BAUD_RATE);
-//   uart_set_format(UART_ID, 8, 1, UART_PARITY_NONE);
+  //   uart_set_format(UART_ID, 8, 1, UART_PARITY_NONE);
   gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
   gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
 
   // Initialize UART1 (uart1) for testing
   uart_init(uart1, BAUD_RATE);
-//   uart_set_format(uart1, 8, 1, UART_PARITY_NONE);
+  //   uart_set_format(uart1, 8, 1, UART_PARITY_NONE);
   gpio_set_function(UART_TX_PIN_TESTING, GPIO_FUNC_UART);
   gpio_set_function(UART_RX_PIN_TESTING, GPIO_FUNC_UART);
 
-  gpio_init(GP15_PIN);
+  //gpio_init(GP15_PIN);
 
   // Set GP15 (GPIO15) as an input pin to read its state.
-  gpio_set_dir(GP15_PIN, GPIO_IN);
-  gpio_set_pulls(GP15_PIN, true, false);
+  //gpio_set_dir(GP15_PIN, GPIO_IN);
+  //gpio_set_pulls(GP15_PIN, true, false);
 
-  while (true) {
-    // test_uart1_tx(); // Call the function to test UART1 communication (TX)
-    // test_uart1_rx(); // Call the function to test UART1 communication (RX)
-    // send_data();     // Call the function to send or receive data based on
-    // GP15's state sleep_ms(1000);   // Wait for 1 second between cycles
-
-    if (scanf("%s", menu_buffer)) {
+  while (true)
+  {
+    if (scanf("%s", menu_buffer))
+    {
       menu_activated = 0;
     }
-    if (menu_activated == 0 && in_menu == 0) {
+    if (menu_activated == 0 && in_menu == 0)
+    {
       print_menu();
       scanf("%s", menu_buffer);
       in_menu = 1;
-      if (strcmp(menu_buffer, "2") == 0) {
-        //selected UART
+      int uart_menu = 0;
+      if (strcmp(menu_buffer, "2") == 0)
+      {
+        // selected UART
 
-        while (1) {
-        if (gpio_get(GP15_PIN)) {
+        printf("1. Sniff\n");
+        printf("2. Inject\n");
+        scanf("%d", &uart_menu);
+
+        if (uart_menu == 1)
+        {
+          while (1)
+          {
+            //test_uart1_tx(); // Test function
+            //sleep_ms(10);
+            sniff();
+            sleep_ms(500); // Wait for 0.5 seconds between cycles
+          }
+        }
+        if (uart_menu == 2)
+        {
           inject();
-          sleep_ms(10);
-          test_uart1_rx(); // Call the function to test UART1 communication (RX) only when GP15 is high
-        }
-        else {
-          test_uart1_tx(); // Call the function to test UART1 communication (TX) only when GP15 is low
-          sleep_ms(10);
-          sniff();
-        }
-        sleep_ms(500); // Wait for 0.5 seconds between cycles
+          //sleep_ms(10);
+          //test_uart1_rx(); // Test function
+          sleep_ms(500);   // Wait for 0.5 seconds between cycles
         }
       }
 
-      if (strcmp(menu_buffer, "3") == 0) {
+      if (strcmp(menu_buffer, "3") == 0)
+      {
         int spi_menu = 0;
         uint8_t message_buffer[1024];
         uint8_t spi0_input_buffer[1024];
-        
-        //not being used for now, need to test the below buffers
-        //uint8_t spi1_input_buffer[1024];
-        //uint8_t spi1_message_buffer[1024];
+
+        // not being used for now, need to test the below buffers
+        // uint8_t spi1_input_buffer[1024];
+        // uint8_t spi1_message_buffer[1024];
 
         init_spi_pins(SPI_PORT, 500000, SPI0_RX_PIN, SPI0_TX_PIN, SPI0_SCLK_PIN, SPI0_CS_PIN);
         init_spi_pins(SPI1_PORT, 500000, SPI1_RX_PIN, SPI1_TX_PIN, SPI1_SCLK_PIN, SPI1_CS_PIN);
@@ -183,17 +206,21 @@ int main() {
         printf("\n2. Inject");
         scanf("%d", &spi_menu);
 
-        if (spi_menu == 1) {
-          while (1) {
-            spi_write_read_blocking(SPI_PORT, spi0_input_buffer,message_buffer, 4); //read from master on left, write to master on left
-            spi_write_read_blocking(SPI1_PORT, message_buffer, spi0_input_buffer, 4); //write to slave on right, read from slave on right
-            //printf("test after func");
-            for (int i = 0; i < 4; i++) {
-            printf("message from spi: %x\n", message_buffer[i]);
+        if (spi_menu == 1)
+        {
+          while (1)
+          {
+            spi_write_read_blocking(SPI_PORT, spi0_input_buffer, message_buffer, 4);  // read from master on left, write to master on left
+            spi_write_read_blocking(SPI1_PORT, message_buffer, spi0_input_buffer, 4); // write to slave on right, read from slave on right
+            // printf("test after func");
+            for (int i = 0; i < 4; i++)
+            {
+              printf("message from spi: %x\n", message_buffer[i]);
             }
           }
         }
-        if (spi_menu == 2) {
+        if (spi_menu == 2)
+        {
           uint8_t injected_input[1024];
           char user_hex_input[1024];
           printf("Enter bytes to send (eg. ff00112233): ");
@@ -201,15 +228,15 @@ int main() {
           int input_len = count_char(user_hex_input);
           size_t test = convert_hex(injected_input, input_len, user_hex_input);
 
-          while (1) {
-              //spi_write_read_blocking(SPI1_PORT, message_buffer, )
-              spi_write_read_blocking(SPI_PORT, spi0_input_buffer, message_buffer, 4); //read from master on left, write to master on left
-              spi_write_read_blocking(SPI1_PORT, injected_input, spi0_input_buffer, 4); //write to slave with injected input on right, read from slave on right
+          while (1)
+          {
+            // spi_write_read_blocking(SPI1_PORT, message_buffer, )
+            spi_write_read_blocking(SPI_PORT, spi0_input_buffer, message_buffer, 4);  // read from master on left, write to master on left
+            spi_write_read_blocking(SPI1_PORT, injected_input, spi0_input_buffer, 4); // write to slave with injected input on right, read from slave on right
           }
         }
       }
     }
-
   }
 
   return 0;
