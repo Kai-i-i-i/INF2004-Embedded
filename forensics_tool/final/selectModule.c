19764ac8544bb885086df9e5f4901cb230013b7b
aleukh <aloysiusyap1999@gmail.com>
2023-11-28 18:58:14 +0800

    integrated SPI sniff and inject

--- forensics_tool/final/selectModule.c
+++ forensics_tool/final/selectModule.c
@@ -4,6 +4,8 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "hardware/uart.h"
+#include "spifuncs/SPIFUNCS.h"
+#include "conversion/conversion.h"
 
 #define SNIFFBUTTON 0
 #define UARTBUTTON 1
@@ -114,6 +116,66 @@ int main(){
                 break;
             case 8:
                 printf("SPI ");
+
+                //init spi spins
+                init_spi_pins(SPI_PORT, 500000, SPI0_RX_PIN, SPI0_TX_PIN, SPI0_SCLK_PIN, SPI0_CS_PIN);
+                init_spi_pins(SPI1_PORT, 500000, SPI1_RX_PIN, SPI1_TX_PIN, SPI1_SCLK_PIN, SPI1_CS_PIN);
+                spi_set_slave(SPI_PORT, true);
+
+                if (injectState) {
+                    //inject
+
+                    // get user input as string
+                    char user_hex_input[1024];
+                    printf("Enter bytes to send (eg. ff00112233): ");
+                    scanf("%s", user_hex_input);
+
+                    // convert the string to actual hex
+                    int input_len = count_char(user_hex_input);
+                    uint8_t *injected_input = create_buffers(input_len);
+                    size_t hexa = convert_hex(injected_input, input_len, user_hex_input);
+
+                    //create the buffers for spi
+                    uint8_t *write_buffer = create_buffers(hexa);
+                    uint8_t *read_buffer = create_buffers(hexa);
+
+                    // writing of injected bytes to slave, and relaying appropriate bytes back to master
+                    while (1) {
+                        inject_spi(write_buffer, read_buffer, injected_input, hexa);
+                        // printing of output/logs
+                        for (int i = 0; i < hexa; i++) {
+                            printf("Messaged from spi0: %x\n", read_buffer[i]);
+                            printf("Messaged writing to spi1: %x\n", injected_input[i]);
+                            printf("Received from spi1: %x\n", write_buffer[i]);
+                            printf("Message write to spi0: %x\n", write_buffer[i]);
+                            printf("\n");
+                        }
+                    }  
+                }
+                else {
+                    //spi sniff
+
+                    //get user injected input
+                    int number_of_bytes_input;
+                    printf("How many bytes of data do you want to sniff at a time?: \n");
+                    scanf("%d", number_of_bytes_input);
+
+                    //create buffers for spi
+                    uint8_t *write_buffer = create_buffers(number_of_bytes_input);
+                    uint8_t *read_buffer = create_buffers(number_of_bytes_input);
+
+                    //inject user input into spi1 and relay appropriate messages between master and slave
+                    while (1) {
+                        sniff_spi(write_buffer, read_buffer, number_of_bytes_input);
+                        //printing of output/logs
+                        for (int i = 0; i < number_of_bytes_input; i++) {
+                            printf("Message from spi0: %x\n", read_buffer[i]);
+                            printf("Message write to spi1: %x\n", read_buffer[i]);
+                            printf("Message from spi1: %x\n", write_buffer[i]);
+                            printf("Message write to spi0: %x\n", write_buffer[i]);
+                            }
+                        }
+                    }
                 break;
             case 0:
                 printf("None plugged\n");
