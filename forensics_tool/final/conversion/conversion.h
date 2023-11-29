0bdc93df321804e1a4010bba7836fdb63b51e572
aleukh <aloysiusyap1999@gmail.com>
2023-11-29 16:58:33 +0800

    addd comments

--- forensics_tool/final/conversion/conversion.h
+++ forensics_tool/final/conversion/conversion.h
@@ -4,8 +4,10 @@
 #include <stdint.h>
 #include <stdio.h>
 
+//function to convert the string hex input into uint8_t
 size_t convert_hex(uint8_t *dest, size_t count, const char *src);
 
+//function to count number of characters in a string
 int count_char(char *user_string);
 
 #endif //SPIFUNCS_H_
